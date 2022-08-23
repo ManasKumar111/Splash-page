59b56935a0481d85e81bd7ef86b65a522f79d07c
Manas Kumar <manasansha@gmail.com>
2022-08-23 17:20:20 +0530

    Splash Page

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello world');
+const {body}=document;
+function changeBackground(number){
+    // Check ifbackground is already showing
+    let previousBackground;
+    if(body.className){
+        previousBackground=body.className;
+    }
+    // Reset CSS class
+    body.className='';
+    switch (number){
+        case '1':
+            return (previousBackground==='background-1' ? false :body.classList.add('background-1'));
+        case '2':
+            return (previousBackground==='background-2' ? false :body.classList.add('background-2'));
+        case '3':
+            return (previousBackground==='background-3' ? false :body.classList.add('background-3'));
+        default:
+            break;    
+    }
+}
